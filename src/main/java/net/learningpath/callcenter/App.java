0860f3019da39dffbd4a2220c90221f3cbafc73c
Juan Florez <juaflore@cobogpgp4860.global.publicisgroupe.net>
2018-02-06 11:06:28 -0500

    initializing project

---
+++ src/main/java/net/learningpath/callcenter/App.java
@@ -0,0 +1,13 @@
+package net.learningpath.callcenter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
