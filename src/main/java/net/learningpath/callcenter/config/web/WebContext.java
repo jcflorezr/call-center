8c188bd25ca13a7fd6e72c9637111ef97c84bba3
Juan Flórez <jcflorezr@gmail.com>
2018-03-15 19:14:30 -0500

    [v2.0] adding spring support for swagger 2 for documenting the rest api

--- src/main/java/net/learningpath/callcenter/config/web/WebContext.java
+++ src/main/java/net/learningpath/callcenter/config/web/WebContext.java
@@ -1,20 +1,24 @@
 package net.learningpath.callcenter.config.web;
 
+import net.learningpath.callcenter.config.root.SwaggerConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("net.learningpath.callcenter.controller")
+@Import(SwaggerConfig.class)
 public class WebContext extends WebMvcConfigurerAdapter {
+
+    public WebContext() {
+        super();
+    }
 
     @Bean
     public ViewResolver viewResolver() {
@@ -33,6 +37,20 @@ public class WebContext extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/docApi/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/docApi/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/docApi/swagger-resources", "/swagger-resources");
     }
 
 }
